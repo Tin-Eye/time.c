d9931e8673821dc3ff2e8f24ed94f1a2ce712a3d
Tin-Eye <altydev19@gmail.com>
2025-07-09 19:39:35 +0200

    add knots.c

---
+++ knots.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int knots;
+
+    printf("Enter a wind speed in knots :");
+    scanf("%d", &knots);
+
+    if (knots < 1) {
+        printf("Calm.\n");
+    }
+    else if (knots <= 3) {
+        printf("Light air\n");
+    }
+    else if (knots <= 27) {
+        printf("Breeze\n");
+    }
+    else if (knots <= 47) {
+        printf("Gale\n");
+    }
+    else if (knots <= 63) {
+        printf("Storm \n");
+    }
+    else {
+        printf("Hurricane \n");
+    }
+
+
+    return 0;
+}
